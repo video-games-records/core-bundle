64b3f3ed8ae7e39126ca550247ad2b4a35e36cf6
magicbart <magicbart@gmail.com>
2018-05-20 08:52:43 +0200

    Add role ROLE_CHART_SUBMIT

--- Controller/GroupController.php
+++ Controller/GroupController.php
@@ -101,7 +101,7 @@ class GroupController extends VgrBaseController
      * @Route("/form/id/{id}", requirements={"id": "[1-9]\d*"}, name="vgr_group_form")
      * @Method("GET")
      * @Cache(smaxage="10")
-     * @Security("is_granted('IS_AUTHENTICATED_REMEMBERED')")
+     * @Security("is_granted('ROLE_CHART_SUBMIT')")
      *
      * @param int $id
      * @return \Symfony\Component\HttpFoundation\Response
