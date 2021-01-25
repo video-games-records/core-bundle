325b94b9d39b5fcd04052f8b73b09a639ef6c9f1
magicbart <magicbart@gmail.com>
2021-01-25 17:41:57 +0100

    Add player chart status translation

---
+++ Entity/PlayerChartStatusTranslation.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace VideoGamesRecords\CoreBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Knp\DoctrineBehaviors\Contract\Entity\TranslationInterface;
+use Knp\DoctrineBehaviors\Model\Translatable\TranslationTrait;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="vgr_player_chart_status_translation")
+ */
+class PlayerChartStatusTranslation implements TranslationInterface
+{
+    use TranslationTrait;
+
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     * @ORM\Column(name="name", type="string", length=255, nullable=false)
+     */
+    private $name;
+
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param string $name
+     * @return $this
+     */
+    public function setName(string $name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+}
