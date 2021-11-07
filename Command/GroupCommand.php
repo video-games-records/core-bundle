5450f398b40d79ff468c2336fadbb69735dd45dd
magicbart <magicbart@gmail.com>
2021-11-07 17:58:26 +0100

    Rafactory group service

--- Command/GroupCommand.php
+++ Command/GroupCommand.php
@@ -1,21 +1,27 @@
 <?php
 namespace VideoGamesRecords\CoreBundle\Command;
 
+use Doctrine\ORM\OptimisticLockException;
+use Doctrine\ORM\ORMException;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Serializer\Exception\ExceptionInterface;
+use VideoGamesRecords\CoreBundle\Service\GroupService;
 
 class GroupCommand extends DefaultCommand
 {
     protected static $defaultName = 'vgr-core:group';
 
-    private $em;
+    private EntityManagerInterface $em;
+    private GroupService $groupService;
 
-    public function __construct(EntityManagerInterface $em)
+    public function __construct(EntityManagerInterface $em, GroupService $groupService)
     {
         $this->em = $em;
+        $this->groupService = $groupService;
         parent::__construct($em);
     }
 
@@ -41,22 +47,24 @@ class GroupCommand extends DefaultCommand
     /**
      * @param InputInterface  $input
      * @param OutputInterface $output
-     * @return bool
+     * @return int
+     * @throws ORMException
+     * @throws OptimisticLockException
+     * @throws ExceptionInterface
      */
-    protected function execute(InputInterface $input, OutputInterface $output)
+    protected function execute(InputInterface $input, OutputInterface $output): int
     {
         $function = $input->getArgument('function');
         switch ($function) {
             case 'maj-player':
                 $idGroup = $input->getOption('idGroup');
-                $this->em->getRepository('VideoGamesRecordsCoreBundle:PlayerGroup')->maj($idGroup);
+                $this->groupService->majPlayerGroup($idGroup);
                 break;
             case 'maj-team':
                 $idGroup = $input->getOption('idGroup');
-                $this->em->getRepository('VideoGamesRecordsCoreBundle:TeamGroup')->maj($idGroup);
+                $this->groupService->majTeamGroup($idGroup);
                 break;
         }
-
-        return true;
+        return 1;
     }
 }
