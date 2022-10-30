b787dc95e8cfa53fe3be93e0df708e9ed58a78ed
magicbart <magicbart@gmail.com>
2022-10-30 19:16:32 +0100

    Refactor : Move command

--- Command/Badge/PlayerBadgeUpdateCommand.php
+++ Command/Badge/PlayerBadgeUpdateCommand.php
@@ -1,5 +1,5 @@
 <?php
-namespace VideoGamesRecords\CoreBundle\Command;
+namespace VideoGamesRecords\CoreBundle\Command\Badge;
 
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
