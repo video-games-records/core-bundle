17f3d2baac0618e0cb8582e8339cbfb39847730a
magicbart <magicbart@gmail.com>
2022-11-25 21:13:01 +0100

    Feat: Use Flysystem for team avatar

---
+++ src/Controller/Team/AvatarController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace VideoGamesRecords\CoreBundle\Controller\Team;
+
+use League\Flysystem\FilesystemException;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\StreamedResponse;
+use Symfony\Contracts\Translation\TranslatorInterface;
+use VideoGamesRecords\CoreBundle\Entity\Team;
+use VideoGamesRecords\CoreBundle\Repository\TeamRepository;
+use VideoGamesRecords\CoreBundle\Service\Team\AvatarManager;
+
+/**
+ * Class TeamController
+ */
+class AvatarController extends AbstractController
+{
+    private TranslatorInterface $translator;
+    private TeamRepository $teamRepository;
+    private AvatarManager $avatarManager;
+
+    private array $extensions = array(
+        'image/png' => '.png',
+        'image/jpeg' => '.jpg',
+    );
+
+    public function __construct(TranslatorInterface $translator, TeamRepository $teamRepository, AvatarManager $avatarManager)
+    {
+        $this->translator = $translator;
+        $this->teamRepository = $teamRepository;
+        $this->avatarManager = $avatarManager;
+    }
+
+    /**
+     * @param Team    $team
+     * @param Request $request
+     * @return Response
+     * @throws FilesystemException
+     */
+    public function upload(Team $team, Request $request): Response
+    {
+        $data = json_decode($request->getContent(), true);
+        $file = $data['file'];
+        $fp1 = fopen($file, 'r');
+        $meta = stream_get_meta_data($fp1);
+        $mimeType = $meta['mediatype'];
+
+        $data = explode(',', $file);
+
+        if (!array_key_exists($meta['mediatype'], $this->extensions)) {
+            return new JsonResponse([
+                'message' => $this->translator->trans('avatar.extension_not_allowed'),
+            ],
+            400
+            );
+        }
+
+        // Set filename
+        $filename = $team->getId() . '_' . uniqid() . '.' . $this->avatarManager->getExtension($mimeType);
+
+        $this->avatarManager->write($filename, base64_decode($data[1]));
+
+        // Save avatar
+        $team->setLogo($filename);
+        $this->teamRepository->flush();
+
+        return new JsonResponse([
+            'message' => $this->translator->trans('avatar.success'),
+        ], 200);
+    }
+
+
+    /**
+     * @param Team $team
+     * @return StreamedResponse
+     * @throws FilesystemException
+     */
+    public function download(Team $team): StreamedResponse
+    {
+        return $this->avatarManager->read($team->getLogo());
+    }
+}
