7ed9e2f24f2f191cf842e8b8eaa530feb4868927
magicbart <magicbart@gmail.com>
2022-12-19 17:48:31 +0100

    Feat: Add tomorrow game of day

--- src/Service/GameOfDayManager.php
+++ src/Service/GameOfDayManager.php
@@ -23,15 +23,15 @@ class GameOfDayManager
      */
     public function add(): void
     {
-        $now = new Datetime();
-        $gameDay = $this->gameDayRepository->findOneBy(array('day' => $now));
+        $tomorrow = new Datetime('tomorrow');
+        $gameDay = $this->gameDayRepository->findOneBy(array('day' => $tomorrow));
         if (!$gameDay) {
             $games = $this->gameRepository->getIds();
             $rand_key = array_rand($games, 1);
             $game = $this->gameRepository->findOneBy($games[$rand_key]);
             $gameDay = new GameDay();
             $gameDay->setGame($game);
-            $gameDay->setDay($now);
+            $gameDay->setDay($tomorrow);
             $this->gameDayRepository->save($gameDay);
             $this->gameDayRepository->flush();
         }
