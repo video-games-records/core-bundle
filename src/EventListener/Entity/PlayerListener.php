21af9d2df5516ffd0e3c59e64503947b0bd4c10c
magicbart <magicbart@gmail.com>
2023-05-29 14:01:03 +0200

    Refactor: Maj chart on edit player.team

--- src/EventListener/Entity/PlayerListener.php
+++ src/EventListener/Entity/PlayerListener.php
@@ -8,17 +8,11 @@ use Doctrine\ORM\ORMException;
 use Doctrine\Persistence\Event\LifecycleEventArgs;
 use VideoGamesRecords\CoreBundle\Entity\Player;
 use VideoGamesRecords\CoreBundle\Service\UpdateChartStatusHandler;
+use VideoGamesRecords\CoreBundle\ValueObject\ChartStatus;
 
 class PlayerListener
 {
     private array $changeSet = array();
-
-    public function __construct(
-        private readonly UpdateChartStatusHandler $updateChartStatusHandler
-    ) {
-
-    }
-
 
     /**
      * @param Player                                 $player
@@ -49,8 +43,13 @@ class PlayerListener
     public function postUpdate(Player $player, LifecycleEventArgs $event): void
     {
         if (array_key_exists('team', $this->changeSet)) {
-            $em = $event->getObjectManager();
-            $this->updateChartStatusHandler->playerMajStatusTeam($player);
+            $em = $event->getObjectManager()->getConnexion();
+            $conn = $em->getConnection();
+            $sql = 'UPDATE vgr_chart
+                SET statusTeam = :status
+                WHERE id IN (SELECT idChart FROM vgr_player_chart WHERE idPlayer = :idPlayer)';
+            $stmt = $conn->prepare($sql);
+            $stmt->executeQuery(['status' => ChartStatus::STATUS_MAJ, 'idPlayer' => $player->getId()]);
             $em->flush();
         }
     }
