3e4f614aba47a39a18095a10a53aca5b03ebdab8
magicbart <magicbart@gmail.com>
2023-07-31 08:22:29 +0200

    Feat: Fetch data youtube video on prePersist

--- src/EventListener/Entity/VideoListener.php
+++ src/EventListener/Entity/VideoListener.php
@@ -4,19 +4,33 @@ namespace VideoGamesRecords\CoreBundle\EventListener\Entity;
 
 use Doctrine\ORM\Exception\ORMException;
 use Doctrine\Persistence\Event\LifecycleEventArgs;
+use Symfony\Component\HttpFoundation\Exception\BadRequestException;
+use Symfony\Contracts\Translation\TranslatorInterface;
+use VideoGamesRecords\CoreBundle\DataProvider\YoutubeProvider;
 use VideoGamesRecords\CoreBundle\Entity\Video;
 use VideoGamesRecords\CoreBundle\Security\UserProvider;
+use VideoGamesRecords\CoreBundle\ValueObject\VideoType;
 
 class VideoListener
 {
     private UserProvider $userProvider;
+    private YoutubeProvider $youtubeProvider;
+    private TranslatorInterface $translator;
 
     /**
-     * @param UserProvider $userProvider
+     * @param UserProvider        $userProvider
+     * @param YoutubeProvider     $youtubeProvider
+     * @param TranslatorInterface $translator
      */
-    public function __construct(UserProvider $userProvider)
-    {
+    public function __construct(
+        UserProvider $userProvider,
+        YoutubeProvider $youtubeProvider,
+        TranslatorInterface $translator
+    ) {
         $this->userProvider = $userProvider;
+        $this->youtubeProvider = $youtubeProvider;
+        $this->translator = $translator;
+
     }
 
     /**
@@ -28,6 +42,27 @@ class VideoListener
     {
         $video->setPlayer($this->userProvider->getPlayer());
         $video->getPlayer()->setNbVideo($video->getPlayer()->getNbVideo() + 1);
+
+        // Set youtube data
+        if ($video->getType()->getValue() === VideoType::TYPE_YOUTUBE) {
+            $response = $this->youtubeProvider->getVideo($video->getVideoId());
+
+            if (count($response->getItems()) == 0) {
+                throw new BadRequestException($this->translator->trans('video.youtube.not_found'));
+            }
+
+            $youtubeVideo = $response->getItems()[0];
+
+            $snippet = $youtubeVideo->getSnippet();
+            $video->setTitle($snippet->getTitle());
+            $video->setThumbnail($snippet->getThumbnails()->getHigh()->getUrl());
+
+            $video->setDescription($snippet->getDescription());
+
+            $statistics = $youtubeVideo->getStatistics();
+            $video->setLikeCount((int) $statistics->getLikeCount());
+            $video->setViewCount((int) $statistics->getViewCount());
+        }
     }
 
     /**
