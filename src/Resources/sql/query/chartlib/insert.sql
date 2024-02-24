f6e040b04d935f104351ff66745157986a3566b2
magicbart <magicbart@gmail.com>
2024-02-24 07:52:54 +0100

    Update chartlib sql

--- src/Resources/sql/query/chartlib/insert.sql
+++ src/Resources/sql/query/chartlib/insert.sql
@@ -1,5 +1,7 @@
 -- Ajout des chartlib pour tout un group
-INSERT INTO vgr_chartlib (idChart,idType,created_at)
-SELECT id,1,NOW()
+INSERT INTO vgr_chartlib (idChart,idType,created_at,updated_at)
+SELECT id,1,NOW(),NOW()
 FROM vgr_chart
 WHERE idGroup = :idGroup;
+
+
