f868243bf69eb95dbb7a681cc618fef40a224eb1
magicbart <magicbart@gmail.com>
2024-06-10 20:09:12 +0200

    use Flysystem for proofs

--- src/Controller/Picture/Show.php
+++ src/Controller/Picture/Show.php
@@ -2,48 +2,48 @@
 
 namespace VideoGamesRecords\CoreBundle\Controller\Picture;
 
-use Aws\S3\Exception\S3Exception;
-use Aws\S3\S3Client;
-use Exception;
+use League\Flysystem\FilesystemException;
+use League\Flysystem\FilesystemOperator;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Cache;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\StreamedResponse;
 use Symfony\Component\Routing\Annotation\Route;
 use VideoGamesRecords\CoreBundle\Entity\Picture;
-use VideoGamesRecords\CoreBundle\File\PictureCreatorFactory;
 
 class Show extends AbstractController
 {
-    private S3Client $s3client;
+    private FilesystemOperator $proofStorage;
 
-    public function __construct(S3Client $s3client)
+    private array $extensions = array(
+        'png' => 'image/png',
+        'jpg' => 'image/jpeg'
+    );
+
+
+    public function __construct(FilesystemOperator $proofStorage)
     {
-        $this->s3client = $s3client;
+        $this->proofStorage = $proofStorage;
     }
 
     /**
      * @Route(path="/proof/picture/{id}", requirements={"id": "[1-9]\d*"}, name="vgr_core_picture_index", methods={"GET"})
      * @Cache(expires="+30 days")
      * @param Picture $picture
-     * @throws Exception
+     * @return StreamedResponse
+     * @throws FilesystemException
      */
-    public function __invoke(Picture $picture): void
+    public function __invoke(Picture $picture): StreamedResponse
     {
-        try {
-            $result = $this->s3client->getObject(
-                [
-                    'Bucket' => $_ENV['AWS_S3_BUCKET_PROOF'],
-                    'Key' => $picture->getPath(),
-                ]
-            );
+        $stream = $this->proofStorage->readStream($picture->getPath());
+        return new StreamedResponse(function() use ($stream) {
+            fpassthru($stream);
+            exit();
+        }, 200, ['Content-Type' => $this->getMimeType($picture->getPath())]);
+    }
 
-            // Display the object in the browser.
-            header("Content-Type: {$result['ContentType']}");
-            echo $result['Body'];
-        } catch (S3Exception $e) {
-            chdir(__DIR__);
-            $picture = PictureCreatorFactory::fromFile('../Resources/img/no_photo.gif');
-            $picture->showPicture('gif');
-        }
-        exit;
+    private function getMimeType(string $file): string
+    {
+        $infos = pathinfo($file);
+        return $this->extensions[$infos['extension']] ?? 'image/png';
     }
 }
