9df5c54b243a3f0eb77e63c7ecd98c5a88c4b1e9
magicbart <magicbart@gmail.com>
2024-09-01 13:47:39 +0200

    Fix get lost position list

--- src/Repository/LostPositionRepository.php
+++ src/Repository/LostPositionRepository.php
@@ -58,9 +58,9 @@ class LostPositionRepository extends DefaultRepository
     {
         $sql = "DELETE vgr_lostposition
         FROM vgr_lostposition
-            INNER JOIN vgr_player_chart ON vgr_lostposition.idPlayer = vgr_player_chart.idPlayer AND vgr_lostposition.idChart = vgr_player_chart.idChart
-        WHERE (vgr_player_chart.rank <= vgr_lostposition.oldRank)
-        OR (vgr_player_chart.rank =1 AND vgr_player_chart.nbEqual = 1 AND vgr_lostposition.oldRank = 0)";
+            INNER JOIN vgr_player_chart ON vgr_lostposition.player_id = vgr_player_chart.player_id AND vgr_lostposition.chart_id = vgr_player_chart.chart_id
+        WHERE (vgr_player_chart.rank <= vgr_lostposition.old_rank)
+        OR (vgr_player_chart.rank = 1 AND vgr_player_chart.nb_equal = 1 AND vgr_lostposition.old_rank = 0)";
         $this->_em->getConnection()->executeStatement($sql);
     }
 
