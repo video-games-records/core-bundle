1d51cee5a3bef303883f78758d840154885c178a
magicbart <magicbart@gmail.com>
2024-09-07 16:23:32 +0200

    Add admin group / game set video proof only form

--- src/Controller/Admin/GroupAdminController.php
+++ src/Controller/Admin/GroupAdminController.php
@@ -11,6 +11,7 @@ use Symfony\Component\HttpFoundation\Response;
 use VideoGamesRecords\CoreBundle\Entity\ChartLib;
 use VideoGamesRecords\CoreBundle\Entity\Group;
 use VideoGamesRecords\CoreBundle\Form\Type\ChartTypeType;
+use VideoGamesRecords\CoreBundle\Form\VideoProofOnly;
 
 class GroupAdminController extends CRUDController
 {
@@ -53,12 +54,17 @@ class GroupAdminController extends CRUDController
      */
     public function addLibChartAction($id, Request $request): RedirectResponse|Response
     {
-        /** @var Group $object */
+        /** @var Group $group */
         $group = $this->admin->getSubject();
 
         if ($group->getGame()->getGameStatus()->isActive()) {
             $this->addFlash('sonata_flash_error', 'Game is already activated');
-            return new RedirectResponse($this->admin->generateUrl('list', ['filter' => $this->admin->getFilterParameters()]));
+            return new RedirectResponse(
+                $this->admin->generateUrl(
+                    'list',
+                    ['filter' => $this->admin->getFilterParameters()]
+                )
+            );
         }
 
         $em = $this->admin->getModelManager()->getEntityManager($this->admin->getClass());
@@ -89,6 +95,44 @@ class GroupAdminController extends CRUDController
                 'object' => $group,
                 'form' => $form,
                 'group' => $group,
+                'action' => 'edit'
+            ]
+        );
+    }
+
+    /**
+     * @param         $id
+     * @param Request $request
+     * @return RedirectResponse|Response
+     */
+    public function setVideoProofOnlyAction($id, Request $request): RedirectResponse|Response
+    {
+        /** @var Group $group */
+        $group = $this->admin->getSubject();
+
+        $em = $this->admin->getModelManager()->getEntityManager($this->admin->getClass());
+        $form = $this->createForm(VideoProofOnly::class);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $data = $form->getData();
+            $isVideoProofOnly = $data['isVideoProofOnly'];
+            foreach ($group->getCharts() as $chart) {
+                $chart->setIsProofVideoOnly($isVideoProofOnly);
+            }
+            $em->flush();
+
+            $this->addFlash('sonata_flash_success', 'All charts are updated successfully');
+            return new RedirectResponse($this->admin->generateUrl('show', ['id' => $group->getId()]));
+        }
+
+        return $this->render(
+            '@VideoGamesRecordsCore/Admin/Form/form.set_video_proof_only.html.twig',
+            [
+                'base_template' => '@SonataAdmin/standard_layout.html.twig',
+                'admin' => $this->admin,
+                'object' => $group,
+                'form' => $form,
+                'title' => $group->getGame()->getName() . ' / ' . $group->getName(),
                 'action' => 'edit'
             ]
         );
