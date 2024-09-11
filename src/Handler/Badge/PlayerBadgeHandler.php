af65a1368deb9e787ac7c047ba7b71c7c7b2f317
magicbart <magicbart@gmail.com>
2024-09-11 12:39:28 +0200

    Fix update query player badge

--- src/Handler/Badge/PlayerBadgeHandler.php
+++ src/Handler/Badge/PlayerBadgeHandler.php
@@ -25,21 +25,21 @@ class PlayerBadgeHandler
         SELECT vgr_player.id,vgr_badge.id, NOW(), NOW()
         FROM vgr_player,pnu_user,vgr_badge
         WHERE type = '%s'
-        AND value <= user.%s
+        AND value <= pnu_user.%s
         AND vgr_player.user_id = pnu_user.id
-        AND vgr_badge.id NOT IN (SELECT idBadge FROM vgr_player_badge WHERE idPlayer = vgr_player.id)";
+        AND vgr_badge.id NOT IN (SELECT badge_id FROM vgr_player_badge WHERE player_id = vgr_player.id)";
 
-        $this->em->getConnection()->executeQuery(sprintf($sql, 'Forum', 'nbForumMessage'));
+        $this->em->getConnection()->executeQuery(sprintf($sql, 'Forum', 'nb_forum_message'));
 
-        $sql = " INSERT INTO vgr_player_badge (player_id, badge_id, created_at, updated_at)
+        $sql = "INSERT INTO vgr_player_badge (player_id, badge_id, created_at, updated_at)
         SELECT vgr_player.id,vgr_badge.id, NOW(), NOW()
         FROM vgr_player,vgr_badge
         WHERE type = '%s'
         AND value <= vgr_player.%s
-        AND vgr_badge.id NOT IN (SELECT idBadge FROM vgr_player_badge WHERE idPlayer = vgr_player.id)";
+        AND vgr_badge.id NOT IN (SELECT badge_id FROM vgr_player_badge WHERE player_id = vgr_player.id)";
 
-        $this->em->getConnection()->executeQuery(sprintf($sql, 'Connexion', 'nbConnexion'));
-        $this->em->getConnection()->executeQuery(sprintf($sql, 'VgrChart', 'nbChart'));
-        $this->em->getConnection()->executeQuery(sprintf($sql, 'VgrProof', 'nbChartProven'));
+        $this->em->getConnection()->executeQuery(sprintf($sql, 'Connexion', 'nb_connexion'));
+        $this->em->getConnection()->executeQuery(sprintf($sql, 'VgrChart', 'nb_chart'));
+        $this->em->getConnection()->executeQuery(sprintf($sql, 'VgrProof', 'nb_chart_proven'));
     }
 }
