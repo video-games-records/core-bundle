74abee1aedebeadd9543d15cc547501009d85e79
magicbart <magicbart@gmail.com>
2025-08-26 15:18:43 +0200

    Add api team_requests

--- src/EventListener/Entity/TeamRequestListener.php
+++ src/EventListener/Entity/TeamRequestListener.php
@@ -4,11 +4,37 @@ declare(strict_types=1);
 
 namespace VideoGamesRecords\CoreBundle\EventListener\Entity;
 
+use Doctrine\ORM\Exception\ORMException;
 use Doctrine\Persistence\Event\LifecycleEventArgs;
+use VideoGamesRecords\CoreBundle\Entity\Team;
 use VideoGamesRecords\CoreBundle\Entity\TeamRequest;
+use VideoGamesRecords\CoreBundle\Security\UserProvider;
 
 class TeamRequestListener
 {
+
+    private UserProvider $userProvider;
+
+    /**
+     * @param UserProvider $userProvider
+     */
+    public function __construct(UserProvider $userProvider)
+    {
+        $this->userProvider = $userProvider;
+    }
+
+
+    /**
+     * @param TeamRequest $teamRequest
+     * @param LifecycleEventArgs $event
+     * @return void
+     * @throws ORMException
+     */
+    public function prePersist(TeamRequest $teamRequest, LifecycleEventArgs $event): void
+    {
+        $teamRequest->setPlayer($this->userProvider->getPlayer());
+    }
+
     /**
      * @param TeamRequest        $teamRequest
      * @param LifecycleEventArgs $event
